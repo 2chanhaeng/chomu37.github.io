9f04d13faca104aea02e98c0a3378a1a15eeb78b
chomu37 <2chanhaeng@gmail.com>
2022-02-09 15:56:07 +0900

    Update script.js

--- hex-link/js/script.js
+++ hex-link/js/script.js
@@ -55,18 +55,7 @@ function changeHex(list, obj){
 
 // 
 function writeHexes(obj){
-    // 오브젝트에서 key를 뽑아 해당 key를 id로 가지는 요소의 center 클래스에 p 요소에 값을 대입해서 기존 내용에 추가
-    for (let id in obj){
-        let hexCenter = document.querySelector(`#${id} .center`)
-        // 만약 이미 p 태그가 있다면 삭제
-        if(hexCenter.querySelector('p')){
-            hexCenter.querySelector('p').remove();
-        }
-        let p = document.createElement("p")
-        p.textContent = obj[id];
-
-        hexCenter.appendChild(p);
-    }
+    null;
 }
 
 function displayPipes(obj){
